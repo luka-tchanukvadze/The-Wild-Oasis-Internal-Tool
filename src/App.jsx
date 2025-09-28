12189489242290cd781b38d999359b1b10906bd9
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-09-28 18:34:35 +0400

    Building More Reusable Styled Components

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import GlobalStyles from "./styles/GlobalStyles";
 import Input from "./ui/Input";
 import Heading from "./ui/Heading";
 import Row from "./ui/Row";
+import Button from "./ui/Button";
 
 const StyledApp = styled.main`
   padding: 20px;
@@ -13,18 +14,20 @@ function App() {
     <>
       <GlobalStyles />
       <StyledApp>
-        <Row type="vertical">
+        <Row>
           <Row type="horizontal">
             <Heading as={"h1"}>The Wild Oasis</Heading>
 
             <div>
               <Heading as={"h2"}>Check in and out</Heading>
-              <button>random 1</button>
-              <button>random 2</button>
+              <Button>random 1</Button>
+              <Button variation="secondary" size="small">
+                random 2
+              </Button>
             </div>
           </Row>
 
-          <Row type="vertical">
+          <Row>
             <Heading as={"h3"}>Form</Heading>
             <form>
               <Input />
