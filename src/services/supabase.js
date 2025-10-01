5d6ec510b5c4c714a06869d85caae7e47a890735
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-10-01 17:51:28 +0400

    Uploading Images to Supabase

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,5 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
-const supabaseUrl = "https://dkausynrztqdjplgkexu.supabase.co";
+export const supabaseUrl = "https://dkausynrztqdjplgkexu.supabase.co";
 const supabaseKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRrYXVzeW5yenRxZGpwbGdrZXh1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTkxNTMyNDcsImV4cCI6MjA3NDcyOTI0N30.DcWQ3kgmMAakydJ96ieWHWSLl_P4WpsMYPvuRSuDnS0";
 const supabase = createClient(supabaseUrl, supabaseKey);
