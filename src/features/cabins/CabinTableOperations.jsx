57f912f8ca241b40db9ce184b90eae711dedc8d1
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-10-04 15:21:20 +0400

    Client-Side Filtering Filtering Cabins

---
+++ src/features/cabins/CabinTableOperations.jsx
@@ -0,0 +1,11 @@
+import Filter from "../../ui/Filter";
+import TableOperations from "../../ui/TableOperations";
+
+function CabinTableOperations() {
+  return (
+    <TableOperations>
+      <Filter />
+    </TableOperations>
+  );
+}
+export default CabinTableOperations;
