f1cdd364dcb8549dfc3b13be3c94ea260a786559
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-10-16 18:33:37 +0400

    Building the Bookings Table

--- src/services/apiBookings.js
+++ src/services/apiBookings.js
@@ -5,12 +5,12 @@ export async function getBookings() {
   const { data, error } = await supabase
     .from("bookings")
     .select(
-      "id, created_at, starData,endDate, numNights, numGuests, status, totalPrice, cabins(name), guests(fullName)"
+      "id, created_at, startDate, endDate, numNights, numGuests, status, totalPrice, cabins(name), guests(fullName)"
     );
 
   if (error) {
-    console.log(error);
-    throw new Error("Bookings could not be created");
+    console.error("Error fetching bookings:", error.message);
+    throw new Error("Bookings could not be loaded");
   }
 
   return data;
