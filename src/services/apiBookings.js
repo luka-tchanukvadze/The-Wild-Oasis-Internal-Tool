3b1f5280761a46e807b4b07468952d42ec4eacad
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-10-18 19:56:16 +0400

    API-Side Filtering Filtering Bookings

--- src/services/apiBookings.js
+++ src/services/apiBookings.js
@@ -6,7 +6,8 @@ export async function getBookings() {
     .from("bookings")
     .select(
       "id, created_at, startDate, endDate, numNights, numGuests, status, totalPrice, cabins(name), guests(fullName, email)"
-    );
+    )
+    .eq("status", "unconfirmed");
 
   if (error) {
     console.error("Error fetching bookings:", error.message);
